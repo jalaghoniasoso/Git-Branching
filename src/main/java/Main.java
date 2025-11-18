771a9075fc9fbf798ea1cc32270300fea19987ff
User <user@Users-MacBook-Air.local>
2025-11-19 01:18:38 +0400

    Something was not done.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Added main function");
     }
+    public static void doNotDoSomething(){
+        System.out.println("Something was not done.");
+    }
 }
